e48dd90a6760b9703b1bb3d431617f80226613cc
crypto <bathingasian001@gmail.com>
2022-08-21 12:10:23 +0900

    fixed some scripts

--- run.sh
+++ run.sh
@@ -2,9 +2,13 @@
 
 # This a startup script for blog web site.
 # Run this at the root of the app directory.
-# NODE_ENV is already set to "production" on my server.
+
+# This script assumes...
+# 1.NODE_ENV is already set to "production" on my server.
+# 2.express app `server` is running using pm2.
 
 cd ./client
 npm run build
 cd ../
+pm2 stop server
 pm2 restart server
